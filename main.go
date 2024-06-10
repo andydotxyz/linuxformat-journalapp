55ba696fb7c08317c7a34dcd235e8636952dae59
Andy Williams <andy@andy.xyz>
2024-06-10 10:41:24 +0100

    Part 2

--- main.go
+++ main.go
@@ -6,32 +6,80 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
 const dateFormat = "2 Jan 2006"
 
-var entries = make(map[string]string)
-
 func main() {
-	a := app.New()
+	a := app.NewWithID("com.example.journal")
 	w := a.NewWindow("My Journal")
+	w.SetPadded(false)
+
 	var date time.Time
 
 	entry := widget.NewMultiLineEntry()
-	entry.OnChanged = func(s string) {
-		dateStr := date.Format(dateFormat)
-		entries[dateStr] = s
-	}
 	title := widget.NewLabel("Today")
 	title.Alignment = fyne.TextAlignCenter
+
+	var updateMoods func()
+	setMood := func(mood string) {
+		dateStr := date.Format(dateFormat)
+		a.Preferences().SetString(dateStr+".mood", mood)
+		updateMoods()
+	}
+
+	mood := container.NewHBox(
+		widget.NewButton("😀", func() {
+			setMood("happy")
+		}),
+		widget.NewButton("😍", func() {
+			setMood("loved")
+		}),
+		widget.NewButton("😟", func() {
+			setMood("sad")
+		}),
+		widget.NewButton("😴", func() {
+			setMood("tired")
+		}),
+		widget.NewButton("😬", func() {
+			setMood("stressed")
+		}),
+	)
+	updateMoods = func() {
+		setSelected := func(o fyne.CanvasObject, high bool) {
+			b, ok := o.(*widget.Button)
+			if !ok {
+				return
+			}
+
+			if high {
+				b.Importance = widget.HighImportance
+			} else {
+				b.Importance = widget.MediumImportance
+			}
+			b.Refresh()
+		}
+
+		dateStr := date.Format(dateFormat)
+		dayMood := a.Preferences().String(dateStr + ".mood")
+		setSelected(mood.Objects[0], dayMood == "happy")
+		setSelected(mood.Objects[1], dayMood == "loved")
+		setSelected(mood.Objects[2], dayMood == "sad")
+		setSelected(mood.Objects[3], dayMood == "tired")
+		setSelected(mood.Objects[4], dayMood == "stressed")
+	}
 
 	setDate := func(d time.Time) {
 		date = d
 		dateStr := date.Format(dateFormat)
 		title.SetText(dateStr)
-		entry.SetText(entries[dateStr])
+		entry.Bind(binding.BindPreferenceString(dateStr, a.Preferences()))
+		entry.Validator = nil
+
+		updateMoods()
 	}
 	setDate(time.Now())
 
@@ -43,7 +91,7 @@ func main() {
 	})
 	bar := container.NewBorder(nil, nil, prev, next, title)
 
-	w.SetContent(container.NewBorder(bar, nil, nil, nil, entry))
-	w.Resize(fyne.NewSize(200, 180))
+	w.SetContent(container.NewBorder(bar, container.NewCenter(mood), nil, nil, entry))
+	w.Resize(fyne.NewSize(240, 210))
 	w.ShowAndRun()
 }
